49efe5f342335942b545f5c9d173be1ecb4b55d6
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-03-03 17:27:50 +0400

    Add / Update Modules

--- liferay/configs/common/scripts/deploy.sh
+++ liferay/configs/common/scripts/deploy.sh
@@ -4,7 +4,9 @@ DEPLOYDIR=/opt/liferay/deploy
 mkdir -p $TMPDIR
 ​
 ### Add Modules to be Deployed Here: ###
+
 modules=(
+	"https://github.com/lfrsales/site-se-site-initializer/releases/download/v0.0.1/com.liferay.site.se.site.initializer-1.0.0.jar"
     "https://github.com/ethib137/loan-calculator/releases/download/v0.0.3/loan-calculator-1.0.0.jar"
     "https://github.com/jverweijL/Markdown/releases/download/v0.4/com.liferay.demo.markdown.api-1.0.0.jar"
     "https://github.com/jverweijL/Markdown/releases/download/v0.4/com.liferay.demo.markdown.service-1.0.0.jar"
@@ -24,14 +26,14 @@ modules=(
     "https://github.com/lfrsales/73-formed-theme/releases/download/v0.0.2/73-formed-theme.war"
     "https://github.com/lfrsales/article-service-template-context-contributor/releases/download/v0.0.1/com.liferay.demo.asset.article.service.template.context.contributor-1.0.0.jar"
     "https://github.com/lfrsales/character-limited-text-input-ddm-form-field/releases/download/v0.0.1/com.liferay.dynamic.data.mapping.form.field.type.internal.character.limited.text-1.0.0.jar"
-    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.2/billboard.theme.contributor-1.0.0.jar"
-    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.2/com.liferay.fragment.collection.contributor.billboard-1.0.0.jar"
-    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.2/com.liferay.fragment.collection.contributor.formed.components-1.0.0.jar"
-    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.2/com.liferay.fragment.collection.contributor.formed.content.display-1.0.0.jar"
-    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.2/com.liferay.fragment.collection.contributor.header.components-1.0.0.jar"
-    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.2/com.liferay.fragment.collection.contributor.integrations-1.0.0.jar"
-    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.2/com.liferay.fragment.collection.contributor.utilities-1.0.0.jar"
-    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.2/d3.theme.contributor-1.0.0.jar"
+    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.3/billboard.theme.contributor-1.0.0.jar"
+    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.3/com.liferay.fragment.collection.contributor.billboard-1.0.0.jar"
+    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.3/com.liferay.fragment.collection.contributor.formed.components-1.0.0.jar"
+    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.3/com.liferay.fragment.collection.contributor.formed.content.display-1.0.0.jar"
+    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.3/com.liferay.fragment.collection.contributor.header.components-1.0.0.jar"
+    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.3/com.liferay.fragment.collection.contributor.integrations-1.0.0.jar"
+    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.3/com.liferay.fragment.collection.contributor.utilities-1.0.0.jar"
+    "https://github.com/lfrsales/demo-fragment-collections/releases/download/v0.0.3/d3.theme.contributor-1.0.0.jar"
     "https://github.com/lfrsales/toggle-control-menu-personal-menu-entry/releases/download/v0.0.1/com.liferay.users.admin.web.internal.product.navigation.personal.menu.toggle.control.menu-1.0.0.jar"
     "https://github.com/martin-dominguez/liferay-modules/releases/download/1.0.0/com.liferay.dynamic.data.mapping.form.field.type.internal.userdata.field-1.0.0.jar"
 )
